f1ac1d5d6e7895a87761b609aa541f73a80310b5
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-26 20:57:02 +0000

    Save

---
+++ Task_4/functions.py
@@ -0,0 +1,88 @@
+from constants import *
+
+# ЗАВДАННЯ
+# Логіка команд реалізована в окремих функціях і ці функції приймають на вхід один або декілька рядків та повертають рядок.
+
+
+def add_contact_input_error(func):
+    def inner(*args, **kwargs):
+        try: return func(*args, **kwargs)
+        except: return ERROR + "Give me name and phone please"
+
+    return inner
+
+
+def read_file_error(func):
+    def inner(*args, **kwargs):
+        try: return func(*args, **kwargs)
+        except: return ERROR + "File Not Found"
+
+    return inner
+
+
+def read_contact_input_error(func):
+    def inner(*args, **kwargs):
+        try: return func(*args, **kwargs)
+        except: return ERROR + "Give me correct name please"
+
+    return inner
+
+
+# Функція для парсингу данних
+def parse_input(user_input: str) -> tuple:
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+# Читаємо файл з контактами
+def read_file() -> dict:
+    contacts = {}
+
+    # Читаємо файл
+    with open('contacts.txt', 'r') as file:
+        # Робимо список зі строк
+        lines = file.readlines()
+    
+    # Парсимо строку
+    for line in lines:
+        line = line.strip().split()
+
+        # Додаємо строку до словника
+        contacts[line[0]] = line[1]
+
+    # Повертаємо словник
+    return contacts
+
+@add_contact_input_error
+# Функція для додавання та модифікації словника
+def add_contact(name: str, number: str) -> str:
+    # Читаємо файл та присвоюємо його значення в словник
+    contacts = read_file()
+    # Додаємо наш контакт до словника або замінюємо якщо є
+    contacts[name] = number
+    # Записуємо словник в файл
+    with open('contacts.txt', 'w') as file:
+        for key, value in contacts.items():    
+            file.write(f'{key} {value}\n')
+
+    # Повертаємо успішне виконання
+    return BOT + 'Successful!'
+
+
+@read_contact_input_error    
+@read_file_error
+# Функція Для читання контактів
+def read_contact(name: str) -> str:
+    # Читаємо файл та присвоюємо його значення в словник
+    contacts = read_file()
+
+    # Перевіряємо чи шукаємо ми один контакт чи всі
+    if not name:
+        text = BOT + 'Contacts:\n'
+        for key, value in contacts.items(): 
+            text += f'{key} {value}'
+            if key != list(contacts.keys())[-1]: text += '\n'
+        return text
+    
+    # Повертаємо імя яке шукали
+    return BOT + f'{name} {contacts[name]}'
