d4050a6e5351b6ef9ebfb80003ddcfaa0234f4c2
Python1Student <108671736+Python1Student@users.noreply.github.com>
2024-02-26 15:11:41 +0000

    save

--- Task_3/main.py
+++ Task_3/main.py
@@ -1,6 +1,9 @@
-print(f"{'Рівень логування':>17} | {'Кількість':^10}")
-print('-' * 18 + '|' + '-' * 11)
-print(f"{'INFO':^17} | {'4':^10}")
-print(f"{'DEBUG':^17} | {'3':^10}")
-print(f"{'ERROR':^17} | {'2':^10}")
-print(f"{'WARNING':^17} | {'1':^10}")
+from sys import argv
+from collections import Counter
+
+
+def parse_log_line(line: str) -> dict:
+    line = line.split(' ', 3)
+    return {'date': line[0], 'time': line[1], 'level': line[2], 'message': line[3]}
+
+print(parse_log_line('2024-01-22 08:30:01 INFO User logged in successfully.'))
