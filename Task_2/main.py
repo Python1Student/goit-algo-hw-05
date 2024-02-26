710ba0bca98a522d0d777dc4e74efbbe1fd518fd
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-26 20:18:58 +0000

    Comments

--- Task_2/main.py
+++ Task_2/main.py
@@ -1,14 +1,11 @@
-# Створємо генератор який оброблює переданий текст
-def generator_numbers(text: str) -> float: # type: ignore
-    # Перебираємо слова з тексту
-    for word in text.split():
-        # Перевіряємо чи слово є число
-        if word.split('.')[0].isdigit(): 
-            # Повертаємо число
-            yield float(word)
 
-# Створюємо функцію підрахунку доходу
-def sum_profit(text: str, func: callable) -> float:
+def generator_numbers(text: str) -> float:  # Створємо генератор який оброблює переданий текст # type: ignore
+    for word in text.split(): # Перебираємо слова з тексту
+        if word.split('.')[0].isdigit(): # Перевіряємо чи слово є число
+            yield float(word) # Повертаємо число
+
+
+def sum_profit(text: str, func: callable) -> float: # Створюємо функцію підрахунку доходу
     return sum(func(text), 0.0)
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
