f17ce22bfc4d7203883e2237264eebbbf4729105
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-26 10:34:51 +0000

    Save

--- Task_2/main.py
+++ Task_2/main.py
@@ -1,7 +1,7 @@
 def generator_numbers(text: str) -> float:
     text = text.split()  
     for word in text:
-        if word.split('.')[0].isdigit():
+        if word.split('.')[0].isdigit(): 
             yield float(word)
 
 def sum_profit(text: str, func: callable) -> float:
@@ -10,3 +10,4 @@ def sum_profit(text: str, func: callable) -> float:
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
+
